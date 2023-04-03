dc446eaa539d7cfbfd4d5964e0fafc89795439bb
JakubSzukala <burski09@outlook.com>
2023-04-03 12:51:15 +0200

    Add generation of random root points for each cell

--- Assets/VoronoiDiagramScript.cs
+++ Assets/VoronoiDiagramScript.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class VoronoiDiagramScript : MonoBehaviour
 {
     private RawImage img;
     private int imgSize;
+    private Vector2Int[] rootPoints;
 
+    public uint rootPointsN;
 
     private void Awake()
     {
@@ -24,29 +27,37 @@ public class VoronoiDiagramScript : MonoBehaviour
 
     public void GenerateDiagram()
     {
+        // Create a white background
         Texture2D texture = new Texture2D(imgSize, imgSize);
         texture.filterMode = FilterMode.Point;
         for (int i = 0; i < imgSize; i++)
         {
             for (int j = 0; j < imgSize; j++)
             {
-                float clr = Random.Range(0, 1f);
+                //float clr = Random.Range(0, 1f);
+                float clr = 1f;
                 texture.SetPixel(i, j, new Color(clr, clr, clr));
             }
         }
+
+        // Generate and place root points
+        GenerateRootPoints(rootPointsN);
+        foreach(var point in rootPoints)
+        {
+            texture.SetPixel(point.x, point.y, new Color(0f, 0f, 0f));
+        }
+
         texture.Apply(); // Copy from CPU to GPU memory to render
         img.texture = texture;
     }
 
 
-    private void GenerateRootPoints()
+    private void GenerateRootPoints(uint n)
     {
-
-    }
-
-
-    private void GenerateUniformRootPoints()
-    {
-        throw new System.NotImplementedException();
+        rootPoints = new Vector2Int[n];
+        for (int i = 0; i < rootPoints.Length; i++)
+        {
+            rootPoints[i] = new Vector2Int(Random.Range(0, imgSize), Random.Range(0, imgSize));
+        }
     }
 }
