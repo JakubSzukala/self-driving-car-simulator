8f6a4fa6d3750b0f4699ff575a446c86081c2dff
JakubSzukala <burski09@outlook.com>
2023-04-11 21:06:48 +0200

    Add random points generation and drawing to canvas

--- Assets/RaceTrackGenerator.cs
+++ Assets/RaceTrackGenerator.cs
@@ -1,30 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+[System.Serializable]
 public class RaceTrackGenerator : MonoBehaviour
 {
+    public RaceTrackGeneratorModel model;
+    public RaceTrackGeneratorView view;
+
+    private RawImage map;
+    [SerializeField] public int mapSizeX = 100;
+    [SerializeField] public int mapSizeY = 100;
+
     void Start()
     {
+        // Create refs
+        model = new RaceTrackGeneratorModel(mapSizeX, mapSizeY);
+        view = new RaceTrackGeneratorView();
+        map = GetComponent<RawImage>();
 
+        // Draw to the texture
+        Texture2D texture = new Texture2D(mapSizeX, mapSizeY);
+        texture.filterMode = FilterMode.Point;
+        view.DrawPoints(model.points, texture, true);
+        map.texture = texture;
     }
 
     void Update()
     {
 
     }
+
+    public void OnClick()
+    {
+        // Track generation
+        model.GenerateTrack();
+
+        // Views update
+        Texture2D texture = new Texture2D(mapSizeX, mapSizeY);
+        texture.filterMode = FilterMode.Point;
+
+        // Draw points
+        view.DrawPoints(model.points, texture, true);
+        map.texture = texture;
+    }
 }
 
-[System.Serializable]
 public class RaceTrackGeneratorModel
 {
-    public int numberOfPoints;
-    public int rangeX;
-    public int rangeY;
+    [SerializeField] public int numberOfPoints = 3;
+    public Vector2[] points;
+    private int rangeX;
+    private int rangeY;
+
+    public RaceTrackGeneratorModel(int rangeX, int rangeY)
+    {
+        this.rangeX = rangeX;
+        this.rangeY = rangeY;
+        points = GenerateRandomPoints(numberOfPoints);
+    }
 
     public void GenerateTrack()
     {
-
+        points = GenerateRandomPoints(numberOfPoints);
     }
 
     private Vector2[] GenerateRandomPoints(int numberOfPoints)
@@ -35,7 +75,7 @@ public class RaceTrackGeneratorModel
             return null;
         }
 
-        Vector2 [] points = new Vector2[numberOfPoints];
+        points = new Vector2[numberOfPoints];
         for(int i = 0; i < numberOfPoints; i++)
         {
             points[i] = new Vector2(Random.Range(0, rangeX), Random.Range(0, rangeY));
@@ -48,5 +88,20 @@ public class RaceTrackGeneratorModel
 
 public class RaceTrackGeneratorView
 {
+    public void DrawPoints(Vector2[] points, Texture2D texture, bool whiteBg)
+    {
+        if(whiteBg)
+        {
+            Color[] whiteBackground = new Color[texture.width * texture.height];
+            System.Array.Fill<Color>(whiteBackground, new Color(1f, 1f, 1f));
+            texture.SetPixels(0, 0, texture.width, texture.height, whiteBackground);
+            texture.Apply();
+        }
 
+        for(int i = 0; i < points.Length; i++)
+        {
+            texture.SetPixel((int)points[i].x, (int)points[i].y, Color.black);
+        }
+        texture.Apply();
+    }
 }
