55b60c9b5195a9d33d13f762abf80d9c3cc5d6ed
JakubSzukala <burski09@outlook.com>
2023-04-18 18:14:33 +0200

    Abstract road mesh generation and add abstract class for mesh generation

--- Assets/RaceTrackGenerator3DView.cs
+++ Assets/RaceTrackGenerator3DView.cs
@@ -22,20 +22,17 @@ public class RaceTrackGenerator3DView : MonoBehaviour, IRaceTrackRenderer
 
     public void RenderTrack(Vector2[] path)
     {
-        road.GetComponent<MeshFilter>().mesh = GenerateRoadMesh(path);
+        Mesh roadMesh;
+        GenerateRoadMesh(path, out roadMesh);
+
+        road.GetComponent<MeshFilter>().mesh = roadMesh;
         road.GetComponent<MeshRenderer>().material = materialRoad;
     }
 
-    private Mesh GenerateRoadMesh(Vector2[] path)
+    private void GenerateRoadMesh(Vector2[] path, out Mesh roadMesh)
     {
         // https://www.youtube.com/watch?v=Q12sb-sOhdI&list=PLFt_AvWsXl0d8aDaovNztYf6iTChHzrHP&index=6
-        // For points with index > 0 take the average of directions
-        // between next and previous to make the path smoother
-        Vector3[] vertices = new Vector3[path.Length * 2];
-        Vector2[] uvs = new Vector2[vertices.Length];
-        int[] meshTriangles = new int[2 * path.Length * 3];
-        int vertexIndex = 0;
-        int triangleIndex = 0;
+        RoadVertexGenerator roadVertexGenerator = new RoadVertexGenerator(path.Length, roadWidth);
         for (int i = 0; i < path.Length; i++)
         {
             int nextIndex = (i + 1) % path.Length;
@@ -48,47 +45,116 @@ public class RaceTrackGenerator3DView : MonoBehaviour, IRaceTrackRenderer
             forward += (Vector3)path[nextIndex] - (Vector3)path[i];
             forward.Normalize();
 
-            // Get the orthogonal points **directions**
-            Vector3 left = new Vector3(forward.y, -forward.x) * 0.5f * roadWidth;
-            Vector3 right = new Vector3(-forward.y, forward.x) * 0.5f * roadWidth;
-
-            // Actual orthogonal points, that will be vertices of road mesh
-            vertices[vertexIndex] = (Vector3)path[i] + left;
-            vertices[vertexIndex + 1] = (Vector3)path[i] + right;
-
-            // Map each vertex to the point on the texture
-            float completionPercentage = i / (float)(path.Length - 1);
-            float v = -1 - Mathf.Abs(2 * completionPercentage - 1);
-            uvs[vertexIndex] = new Vector2(0, v);
-            uvs[vertexIndex + 1] = new Vector2(1, v);
-
-            // Assign each vertex to a mesh triangle (see video)
-            // Each three members of meshTriangles constitute to vertices of one triangle
-            meshTriangles[triangleIndex] = vertexIndex;
-            meshTriangles[triangleIndex + 1] = (vertexIndex + 2) % vertices.Length;
-            meshTriangles[triangleIndex + 2] = vertexIndex + 1;
-
-            meshTriangles[triangleIndex + 3] = vertexIndex + 1;
-            meshTriangles[triangleIndex + 4] = (vertexIndex + 2) % vertices.Length;
-            meshTriangles[triangleIndex + 5] = (vertexIndex + 3) % vertices.Length;
-
-            vertexIndex += 2;
-            triangleIndex += 6;
+            roadVertexGenerator.AddVertices(path[i], forward);
         }
-        Mesh mesh = new Mesh();
+        roadMesh = roadVertexGenerator.GetMesh();
+    }
+}
+
+
+public abstract class RaceTrackVertexGenerator
+{
+    protected int pathLength;
+    protected Vector3[] vertices;
+    protected Vector2[] uvs;
+    protected int[] triangles;
+    protected int vertexIndex;
+    protected int triangleIndex;
+    protected float completionPercentage;
+    protected Mesh mesh;
+
+    public RaceTrackVertexGenerator(int pathLength)
+    {
+        this.pathLength = pathLength;
+        this.vertices = new Vector3[2 * pathLength];
+        this.uvs = new Vector2[this.vertices.Length];
+        this.triangles = new int[2 * pathLength * 3];
+        this.vertexIndex = 0;
+        this.triangleIndex = 0;
+        this.completionPercentage = 0;
+    }
+
+    public abstract void AddVertices(Vector2 pathPoint, Vector2 forward);
+
+    public abstract Mesh GetMesh();
+
+    public abstract void Reset();
+
+    public abstract void SetPathLength(int pathLength);
+}
+
+
+public class RoadVertexGenerator : RaceTrackVertexGenerator
+{
+    private float roadWidth;
+
+    public RoadVertexGenerator(int pathLength, float roadWidth) : base(pathLength)
+    {
+        this.roadWidth = roadWidth;
+    }
+
+    public override void AddVertices(Vector2 pathPoint, Vector2 forward)
+    {
+        // Calculate orthogonal points on both sides
+        Vector3 left = new Vector3(forward.y, -forward.x) * 0.5f * roadWidth;
+        Vector3 right = new Vector3(-forward.y, forward.x) * 0.5f * roadWidth;
+
+        // Add them as vertices
+        vertices[vertexIndex] = (Vector3)pathPoint + left;
+        vertices[vertexIndex + 1] = (Vector3)pathPoint + right;
+
+        // Set uvs, take into account completion percentage of entire track
+        completionPercentage += 1 / (float)(pathLength - 1);
+        float v = -1 - Mathf.Abs(2 * completionPercentage - 1);
+        uvs[vertexIndex] = new Vector2(0, v);
+        uvs[vertexIndex + 1] = new Vector2(1, v);
+
+        triangles[triangleIndex] = vertexIndex;
+        triangles[triangleIndex + 1] = (vertexIndex + 2) % vertices.Length;
+        triangles[triangleIndex + 2] = vertexIndex + 1;
+
+        triangles[triangleIndex + 3] = vertexIndex + 1;
+        triangles[triangleIndex + 4] = (vertexIndex + 2) % vertices.Length;
+        triangles[triangleIndex + 5] = (vertexIndex + 3) % vertices.Length;
+
+        vertexIndex += 2;
+        triangleIndex += 6;
+    }
+
+    public override Mesh GetMesh()
+    {
+        mesh = new Mesh();
         mesh.vertices = vertices;
-        mesh.triangles = meshTriangles;
+        mesh.triangles = triangles;
         mesh.uv = uvs;
         return mesh;
     }
 
-    private void AddRoadVertex(Vector3[] vertices, Vector2[] uvs, int[] triangles, int vIdx, int tIdx, Vector2 forward)
+    public override void Reset()
     {
-
+        this.vertices = new Vector3[2 * pathLength];
+        this.uvs = new Vector2[this.vertices.Length];
+        this.triangles = new int[2 * pathLength * 3];
+        this.vertexIndex = 0;
+        this.triangleIndex = 0;
+        this.completionPercentage = 0;
     }
 
-    private void AddWallVertex(Vector3[] vertices, Vector2[] uvs, int[] triangles, int vIdx, int tIdx, Vector2 forward, bool isLeft)
+    public override void SetPathLength(int pathLength)
     {
-        
+        if (pathLength < 1)
+        {
+            throw new System.ArgumentException("Path length can not be smaller than 1.");
+        }
+        this.pathLength = pathLength;
+    }
+
+    public void SetRoadWidth(float roadWidth)
+    {
+        if (roadWidth < 0f)
+        {
+            throw new System.ArgumentException("Road width can not be smaller than 0.");
+        }
+        this.roadWidth = roadWidth;
     }
 }
