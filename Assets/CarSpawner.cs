a227dda9292bc8e8ffb8d6f90c9d775e0627a5cb
JakubSzukala <burski09@outlook.com>
2023-05-02 09:30:28 +0200

    Add camera following for debugging

--- Assets/CarSpawner.cs
+++ Assets/CarSpawner.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject carPrefab;
+    [SerializeField] private GameObject mainCamera;
 
     public IEnumerator spawnCar(Vector3 position, Vector3 direction)
     {
         yield return new WaitForEndOfFrame();
         GameObject newCar = Instantiate(carPrefab, position, Quaternion.LookRotation(direction));
+
+        // If camera has a follow script attach it to the car
+        AWSIM.FollowCamera cameraFollower = mainCamera.GetComponent<AWSIM.FollowCamera>();
+        if (cameraFollower)
+        {
+            cameraFollower.target = newCar.transform;
+        }
     }
 }
