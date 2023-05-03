1c1524f75269200b31080c38c74788876da09297
JakubSzukala <burski09@outlook.com>
2023-05-03 15:48:36 +0200

    Spawn checkpoints to spawner GameObject so it is more structured

--- Assets/CheckPointSpawner.cs
+++ Assets/CheckPointSpawner.cs
@@ -8,8 +8,8 @@ public class CheckPointSpawner : MonoBehaviour
 
     public IEnumerator SpawnCheckPoint(Vector3 position, Vector3 direction, float width)
     {
-        yield return new WaitForEndOfFrame(); // TODO: Spawn checkpoints to separate GameObject
-        GameObject newCheckPoint = Instantiate(checkPointPrefab, position, Quaternion.LookRotation(direction));
+        yield return new WaitForEndOfFrame();
+        GameObject newCheckPoint = Instantiate(checkPointPrefab, position, Quaternion.LookRotation(direction), transform);
         newCheckPoint.transform.localScale = new Vector3(
             width, newCheckPoint.transform.localScale.y, newCheckPoint.transform.localScale.z);
     }
