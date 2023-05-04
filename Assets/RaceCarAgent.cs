25e76d797deda5e6de5cd0bc2a7b7748d7712615
JakubSzukala <burski09@outlook.com>
2023-05-04 18:32:47 +0200

    Implement CollectObservations method in agent

--- Assets/RaceCarAgent.cs
+++ Assets/RaceCarAgent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using AWSIM.LaserFormat;
+using RGLUnityPlugin;
 
 
 // It will need to be replacement for main controller
@@ -18,19 +20,36 @@ public class RaceCarAgent : Agent
     [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private SimpleCountDownTimer timer;
 
-    void Start()
+    [SerializeField] private GameObject agentCar;
+
+    private LidarDataSubscriber lidarDataSubscriber;
+
+    void Awake()
     {
-        
+        agentCar = GameObject.FindGameObjectWithTag("AgentCar");
+        lidarDataSubscriber = new LidarDataSubscriber(agentCar.GetComponentInChildren<LidarSensor>());
     }
 
     public override void OnEpisodeBegin()
     {
-        // Spawn a car and I guess spawn a track first
+        // Spawn race track
+        raceTrack.CreateRaceTrack(true);
+
+        // Set the agent car at the start
+        Vector3 start, direction;
+        raceTrack.GetRaceTrackStart(out start, out direction);
+        agentCar.transform.position = start;
+        agentCar.transform.rotation = Quaternion.LookRotation(direction);
+
+        // Reset velocities
+        agentCar.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        agentCar.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        agentCar.transform.rotation = Quaternion.LookRotation(direction);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Add Lidar observations to sensor
+        sensor.AddObservation(lidarDataSubscriber.Distances);
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
