681fd6b1d784057ec7cb81d3e02476d9acfb9265
JakubSzukala <burski09@outlook.com>
2023-05-04 23:58:44 +0200

    Immediate spawn of a checkpoint, fix to doubled checkpoints in ml agents environment, work in progress

--- Assets/CheckPointSpawner.cs
+++ Assets/CheckPointSpawner.cs
@@ -7,9 +7,9 @@ public class CheckPointSpawner : MonoBehaviour
     [SerializeField] private GameObject checkPointPrefab;
     [SerializeField] private GameObject checkPointContainer;
 
-    public IEnumerator SpawnCheckPoint(Vector3 position, Vector3 direction, float width)
+    public void SpawnCheckPoint(Vector3 position, Vector3 direction, float width)
     {
-        yield return new WaitForEndOfFrame();
+        //yield return new WaitForEndOfFrame();
         GameObject newCheckPoint = Instantiate(checkPointPrefab, position, Quaternion.LookRotation(direction), checkPointContainer.transform);
         newCheckPoint.transform.localScale = new Vector3(
             width, newCheckPoint.transform.localScale.y, newCheckPoint.transform.localScale.z);
