5416d689d0aaf02d930b539dbebde7de342d698b
JakubSzukala <burski09@outlook.com>
2023-05-07 23:27:09 +0200

    Add ChekcpointCallback for periodic saving

--- python-side-training/gym-train/gym_train.py
+++ python-side-training/gym-train/gym_train.py
@@ -25,14 +25,25 @@ now = datetime.now()
 
 date_string = now.strftime("%d-%m-%Y-%H-%M-%S")
 filename_best = algorithm + "-" + policy + "-" + date_string + "_best"
+filename_checkpoint = algorithm + "-" + policy + "-" + date_string + "checkpoint"
 
 # Eval is in the same environment, track is randomized, no need to change
-eval_callback = EvalCallback(env, best_model_save_path=join("logs", filename_best),
-                             log_path=join("logs", filename_best), eval_freq=1000,
-                             deterministic=True, render=False)
+eval_callback = EvalCallback(env,
+                             best_model_save_path=join("logs", filename_best),
+                             log_path=join("logs", filename_best),
+                             eval_freq=1000,
+                             deterministic=True,
+                             render=False)
+
+checkpoint_callback = CheckpointCallback(
+    save_freq=1000,
+    save_path="logs",
+    name_prefix=filename_checkpoint,
+    verbose=2
+)
 
 # Instantiate and start learning loop
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="tensorboard")
-model.learn(total_timesteps=1500, callback=[eval_callback])
+model.learn(total_timesteps=1500, callback=[eval_callback, checkpoint_callback])
 
 unity_env.close()
